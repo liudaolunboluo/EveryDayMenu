a6659404bb15f516de3888a358000169d0b8cd79
zhangyunfan <yunfanzhang@kuainiugroup.com>
2023-05-06 10:12:20 +0800

    初始化

---
+++ src/main/java/com/liudaolunhuibl/everydaymenu/EveryDayMenuApplication.java
@@ -0,0 +1,13 @@
+package com.liudaolunhuibl.everydaymenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryDayMenuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EveryDayMenuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liudaolunhuibl/everydaymenu/EveryDayMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liudaolunhuibl.everydaymenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveryDayMenuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
